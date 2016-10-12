909cdaefa95df60296b6de8dac536add5a763cc0
Administrator <593128836@qq.com>
2016-10-12 14:23:49 +0800

    add get and set

--- src/chapter6/Employee.java
+++ src/chapter6/Employee.java
@@ -1,30 +1,54 @@
 package chapter6;
 
 public class Employee implements Comparable<Employee> {
-	private int id;
-	private String name;
-	private int age;
+    private int id;
+    private String name;
+    private int age;
 
-	public Employee(int id, String name, int age) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.age = age;
-	}
+    public int getId() {
+        return id;
+    }
 
-	@Override
-	public int compareTo(Employee o) {
-		if (id > o.id) {
-			return 1;
-		} else if (id > o.id) {
-			return -1;
-		}
-		return 0;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", age=" + age + "]";
-	}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Employee(int id, String name, int age) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        if (id > o.id) {
+            return 1;
+        } else if (id > o.id) {
+            return -1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", name=" + name + ", age=" + age + "]";
+    }
 
 }
